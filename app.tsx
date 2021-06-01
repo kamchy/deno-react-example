d012954fc8c6fad521d0d7b98f77768728e72d14
Kamila Chyla <kamila.chyla@gmail.com>
2021-06-01 15:44:43 +0200

    Add simple svg-updating react components for rectangular deno :)

--- app.tsx
+++ app.tsx
@@ -1,42 +1,134 @@
 import { React, ReactDOM } from "./deps.ts";
 
-
 type DenoConfig = {
-  stroke: string,
-  fill: string,
-  width: number, 
-  height: number
-}
-
-type AppData = {
-  title: string,
-  deno: DenoConfig
-}
-
-const Deno = ({width, height, stroke, fill}: DenoConfig) => {
+  stroke: string;
+  fill: string;
+  width: number;
+  height: number;
+};
+const Deno = (dc: DenoConfig) => {
   return (
-    <svg  xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink">
-      <rect x="10" y="10" height={height} width={width}
-          style={{fill, stroke}}/>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <rect
+        x="10"
+        y="10"
+        height={dc.height}
+        width={dc.width}
+        style={{ fill: dc.fill, stroke: dc.stroke }}
+      />
     </svg>
+  );
+};
 
+type InputType = string|number
+interface Prop<T extends InputType> {
+  name: string;
+  inputType: string;
+  val: T
+  onPropUpdated: (val: T) => void;
+}
+
+
+function InputElem<T extends InputType>(p: Prop<T>) {
+  const [val, setVal] = React.useState(p.val)
+  const update = (e:any) => {
+    const val = e.target.value
+    setVal(val)
+    p.onPropUpdated(val)
+  }
+  return (
+    <div>
+      <label>{p.name}</label>
+        <input type={p.inputType} onChange={update} value={val}></input>
+    <label>{val}</label>
+        </div>
   )
 }
-const App = ({title, deno}: AppData) => {
+
+const StrIE = (p: Prop<string>) => InputElem(p)
+const NumIE = (p: Prop<number>) => InputElem(p)
+
+const StringInputElemOld = (p: Prop<string>) => {
+  const [val, setVal] = React.useState(p.val)
+  const update = (e:any) => {
+    const val = e.target.value
+    setVal(val)
+  }
+  return (
+    <div>
+      <label>{p.name}</label>
+        <input type={p.inputType} onInput={update} onChange={()=>p.onPropUpdated(val)} value={val}></input>
+    <label>{val}</label>
+        </div>
+  );
+};
+
+type AppData = {
+  title: string;
+  deno: DenoConfig;
+};
+
+const App = (ad: AppData) => {
+  const [ds, setDs] = React.useState(ad.deno);
+
+  const onDenoDataChange = (news: string) => {
+    console.log("onDenoDataChange called with ", news);
+  };
+
+  const wi: Prop<number> = {
+    name: "Width",
+    inputType: "number",
+    val: ds.width,
+    onPropUpdated: (nv) => setDs({...ds, width: nv}),
+  };
+
+  const hi: Prop<number> = {
+    name: "Height",
+    inputType: "number",
+    val: 200,
+    onPropUpdated: (nv) => setDs({...ds, height: nv}),
+  };
+  
+  const fill: Prop<string> = {
+    name: "Fill",
+    inputType: "color",
+    val: '#ff2233',
+    onPropUpdated: (nv) => setDs({...ds, fill: nv}),
+  };
+
+  const stroke: Prop<string> = {
+    name: "Stroke",
+    inputType: "color",
+    val: '#ff22ff',
+    onPropUpdated: (nv) => setDs({...ds, stroke: nv}),
+  };
+
+
   return (
     <div className="app">
-      <h1>{title}</h1>
-      <Deno {...deno} />
-
+      <h1>{ad.title}</h1>
+      <NumIE {...wi} />
+      <NumIE {...hi} />
+      <StrIE {...fill} />
+      <StrIE {...stroke} />
+      <Deno {...ds} />
     </div>
   );
-}
+};
 
-const appData = {title: "Hello", deno: {width: 100, height: 100, fill: '#ffaabb', stroke: '#66aabb'}}
+const appData: AppData = {
+  title: "Hello",
+  deno: {
+    width: 100,
+    height: 100,
+    fill: "#ffaabb",
+    stroke: "#991100",
+  },
+};
 
 export default function renderApp(rootElem: HTMLElement) {
-    ReactDOM.render(<App {...appData}/>, rootElem);
+  ReactDOM.render(<App {...appData} />, rootElem);
 }
-
-
